664d10e4e704e09e62391321f8112e1885ec37bb
Farzad Yousefzadeh <farskid@gmail.com>
2022-10-12 13:29:28 +0300

    Tutorials script sort by the intended order of content

--- scripts/tutorials.js
+++ scripts/tutorials.js
@@ -1,54 +1,88 @@
 // @ts-check
 
-const fs = require('fs');
-const path = require('path');
-const { promisify } = require('util');
+const fs = require("fs");
+const path = require("path");
+const { promisify } = require("util");
 const readdir = promisify(fs.readdir);
 const stat = promisify(fs.stat);
 
-const fileNames = ['intro.md', 'howtos.md', 'docs-link.md'];
+const baseSortedArray = [
+  "intro.md",
+  "what-is-a-state-machine",
+  "what-is-a-statechart",
+  "states",
+  "initial-states",
+  "transitions-and-events",
+  "parent-states",
+  "parallel-states",
+  "final-states",
+  "history-states",
+  "guards",
+  "eventless-transitions",
+  "delayed-transitions",
+  "state-done-events",
+  "self-transitions",
+  "entry-and-exit-actions",
+  "invoked-actors",
+  "invoke-done-events",
+  "invoke-error-events",
+  "descriptions",
+];
+
+const fileNames = ["intro.md", "howtos.md", "docs-link.md"];
+const tutorialsDir = path.join(__dirname, "..", "copy", "tutorials");
+
+function writeContent(file, fd) {
+  console.log(`Writing ${file}...`);
+  // Write the file to the new tutorials.md file
+  fs.writeSync(fd, fs.readFileSync(file, "utf8"));
+  // Add a couple newlines
+  fs.writeSync(fd, "\n\n");
+}
 
 async function combineTutorials() {
-  console.log('Getting tutorial directories...');
+  console.log("Getting tutorial directories...");
 
   // Get each directory under copy/tutorials
-  const tutorialsDir = path.join(__dirname, '..', 'copy', 'tutorials');
-  const tutorials = await readdir(tutorialsDir);
+  let tutorials = await readdir(tutorialsDir);
 
-  console.log('Writing copy/tutorials.md...');
+  // Sort tutorials-dir content based on the documented order and filter out what's not in there
+  tutorials = tutorials
+    .filter((a) => baseSortedArray.includes(a))
+    .sort(function (a, b) {
+      return baseSortedArray.indexOf(a) - baseSortedArray.indexOf(b);
+    });
+
+  console.log("Writing copy/tutorials.md...");
   // Make a new tutorials.md file
-  const tutorialsMd = path.join(__dirname, '..', 'copy', 'tutorials.md');
-  const fd = fs.openSync(tutorialsMd, 'w');
+  const tutorialsMd = path.join(__dirname, "..", "copy", "tutorials.md");
+  const fd = fs.openSync(tutorialsMd, "w");
 
   // For each tutorial...
   for (const tutorial of tutorials) {
-    // Skip if not a directory
+    // If it's a file
     if (!(await stat(path.join(tutorialsDir, tutorial))).isDirectory()) {
-      console.log(`Skipping ${tutorial} (not a directory)...`);
-      continue;
+      const file = path.join(tutorialsDir, tutorial);
+      writeContent(file, fd);
     }
+    // If it's a directory
+    else {
+      // For each file name in fileNames...
+      for (const fileName of fileNames) {
+        // Read the file
+        const file = path.join(tutorialsDir, tutorial, fileName);
 
-    // For each file name in fileNames...
-    for (const fileName of fileNames) {
-      // Read the file
-      const file = path.join(tutorialsDir, tutorial, fileName);
+        // Check if file exists
+        if (!fs.existsSync(file)) {
+          continue;
+        }
 
-      // Check if file exists
-      if (!fs.existsSync(file)) {
-        continue;
+        writeContent(file, fd);
       }
-
-      // Write the file to the new tutorials.md file
-      console.log(`Writing ${file}...`);
-      const contents = fs.readFileSync(file, 'utf8');
-      fs.writeSync(fd, contents);
-
-      // Add a couple newlines
-      fs.writeSync(fd, '\n\n');
     }
   }
 
-  console.log('Done!');
+  console.log("Done!");
 }
 
 combineTutorials();
